d2660f90f7ba24a886f0d453d4794370d8b285fe
TerraMonster5 <93338246+TerraMonster5@users.noreply.github.com>
2023-01-26 10:56:28 +0000

    Added __del__ function to close database

--- source/sqtoolkit.py
+++ source/sqtoolkit.py
@@ -4,15 +4,18 @@ class DataBase:
     def __init__(self, file: str) -> None:
         self._db = sql.connect(file)
         self._cursor = self._db.cursor()
+    
+    def __del__(self) -> None:
+        self._db.close()
 
     def createTable(self, tableName: str, fields: tuple) -> None:
         pass
 
     def dropTable(self, tableName: str) -> None:
-        self._cursor.execute("DROP TABLE " + tableName)
+        self._cursor.execute(f"DROP TABLE {tableName}")
 
     def insertRecord(self, tableName: str, fields: tuple, values: tuple) -> None:
-        self._cursor.execute("INSERT INTO " + tableName + str(fields))
+        self._cursor.execute(f"INSERT INTO {tableName}{fields} VALUES {values}")
 
     def deleteRecord(self) -> None:
         pass
