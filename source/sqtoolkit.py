55e8c84fddce2d690c3a35c8a34a6e471164364f
TerraMonster5 <william.dargan@gmail.com>
2023-01-31 17:03:00 +0000

    Added quotations around values in insertRecord

--- source/sqtoolkit.py
+++ source/sqtoolkit.py
@@ -9,24 +9,30 @@ class DataBase:
     def __del__(self) -> None:
         self._db.close()
 
-    def createTable(self, tableName: str, fields: tuple[str]) -> None:
+    def createTable(self, tableName: str, fields: tuple) -> None:
         fieldsStr = ",".join(fields)
         self._cursor.execute(f"CREATE TABLE {tableName}({fieldsStr})")
+        self._db.commit()
 
     def dropTable(self, tableName: str) -> None:
         self._cursor.execute(f"DROP TABLE {tableName}")
+        self._db.commit()
 
-    def insertRecord(self, tableName: str, fields: tuple[str], values: tuple[str]) -> None:
-        fieldsStr, valuesStr = ",".join(fields), ",".join(values)
-        self._cursor.execute(f"INSERT INTO {tableName}({fieldsStr}) VALUES {valuesStr}")
+    def insertRecord(self, tableName: str, fields: tuple, values: tuple) -> None:
+        fieldsStr, valuesStr = ",".join(fields), ','.join(f"'{x}'" for x in values)
+        self._cursor.execute(f"INSERT INTO {tableName}({fieldsStr}) VALUES ({valuesStr})")
+        self._db.commit()
 
     def deleteRecord(self, tableName: str, condition: str) -> None:
         self._cursor.execute(f"DELETE FROM {tableName} WHERE {condition}")
+        self._db.commit()
 
-    def dbQuery(self, tableName, fields: str="*", condition: Union[str, None]=None) -> Iterable[tuple[str]]:
+    def dbQuery(self, tableName: str, fields: str="*", condition: Union[str, None]=None, reverse: bool=False) -> Iterable[tuple]:
         query = f"SELECT {fields} FROM {tableName}"
         if condition:
             query += f" WHERE {condition}"
+        if reverse:
+            query += f" ORDER BY DESC"
         self._cursor.execute(query)
         rows = self._cursor.fetchall()
         for row in rows:
